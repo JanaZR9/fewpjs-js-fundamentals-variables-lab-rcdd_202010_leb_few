dd068627d258e0d968885534760eed766ceb9c45
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-11 09:57:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName='Scuber';
 let mostProfitableNeighborhood='Chelsea';
 let companyCeo ='Susan Smith';
+=======
+let companyName = 'Scuber';
+const companyName ='Scuber';
+>>>>>>> ad7d6befba5b1f6b93074951fb695ae4552ba598
